ae84a4fcf9366910f7a163f5dd766064465e481d
Signe <sign4731@stud.kea.dk>
2021-02-09 10:32:43 +0100

    last task

--- script.js
+++ script.js
@@ -45,5 +45,18 @@ function showOutput() {
     const part2 = val.substring(3);
     output.innerHTML = `${part1}${name2}${part2}`;
   } else if (operator == "makeOtherLetterUppercase") {
+    const part1 = val.substring(0, firstSpace + 1);
+    const character = val[firstSpace + 1];
+    const part2 = val.substring(firstSpace + 2);
+    const result1 = part1 + character.toUpperCase() + part2;
+
+    const firstHyphen = result1.indexOf("-");
+    const part1hyphen = result1.substring(0, firstHyphen + 1);
+    const character2 = result1[firstHyphen + 1];
+    const part2hyphen = result1.substring(firstHyphen + 2);
+    const result2 = part1hyphen + character2.toUpperCase() + part2hyphen;
+    console.log(result2);
+
+    output.innerHTML = result2;
   }
 }
